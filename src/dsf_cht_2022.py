d7ab70e3e0b0e9f6f7d514145003eea186395837
fprigent <frederic.prigent@onf.fr>
2022-07-21 10:35:43 +0200

    Signed-off-by: fprigent <frederic.prigent@onf.fr>

--- src/dsf_cht_2022.py
+++ src/dsf_cht_2022.py
@@ -230,7 +230,7 @@ dsf.to_csv(f"{out_path}/{file_name}.csv", sep=';', encoding="latin1",
            quoting=csv.QUOTE_NONNUMERIC)
 dsf.to_file(f"{out_path}/{file_name}.shp.zip", driver='ESRI Shapefile')
 epi = epigdf.drop('REMARQUE', axis=1)
-epi.to_csv(f"{out_path}/dsf_2022_epicollect.csv", index=False, sep=',',
+epigdf.to_csv(f"{out_path}/dsf_2022_epicollect.csv", index=False, sep=',',
               encoding="latin1")
 
 ### statistiques ###############################################################
